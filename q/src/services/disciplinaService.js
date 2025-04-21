31d6f174d44f32a7c25ed9f7ab04cca268bd1284
GaJFerreira <gabrielferreiranunes@live.com>
2025-04-21 17:13:40 -0300

    Separando projeto pra repositorio proprio

--- q/src/services/disciplinaService.js
+++
@@ -1,7 +0,0 @@
-const API_URL = "http://localhost:8080/api/disciplinas"
-
-export async function listarDisciplinas() {
-  const res = await fetch(API_URL)
-  if (!res.ok) throw new Error("Erro ao listar disciplinas")
-  return res.json()
-}
